becbde68ac6bed192b362cc60e94011af50c8816
astankovic <aleksa.stankovic@berkeley.edu>
2018-01-14 16:12:53 +0100

    Add marketplace mockup

--- src/getMockData.js
+++ src/getMockData.js
@@ -2,6 +2,22 @@ import { COMPOSE } from './constants/types';
 
 export const getJingleIdsMock = () => [1, 2, 3, 4, 5, 6];
 
+import img0 from './mockImages/render_0.png';
+import img1 from './mockImages/render_1.png';
+import img2 from './mockImages/render_2.png';
+import img3 from './mockImages/render_3.png';
+import img4 from './mockImages/render_4.png';
+import img5 from './mockImages/render_5.png';
+import img6 from './mockImages/render_6.png';
+import img7 from './mockImages/render_7.png';
+import img8 from './mockImages/render_8.png';
+import img9 from './mockImages/render_9.png';
+import img10 from './mockImages/render_10.png';
+import img11 from './mockImages/render_11.png';
+import img12 from './mockImages/render_12.png';
+import img13 from './mockImages/render_13.png';
+import img14 from './mockImages/render_14.png';
+
 // Put some path resolver for audio source
 export const getJingleFromJingleId = (jingleId) => {
   switch (jingleId) {
@@ -28,4 +44,22 @@ export const getJingleSlots = () => [
   { accepts: [COMPOSE], lastDroppedItem: null },
   { accepts: [COMPOSE], lastDroppedItem: null },
   { accepts: [COMPOSE], lastDroppedItem: null },
+];
+
+export const getSongs = () => [
+  { id: 0, author: 'Saban Saulic', name: 'OFFICIAL', imageSrc: img0 },
+  { id: 1, author: 'Ceca', name: 'Beograd', imageSrc: img1 },
+  { id: 2, author: 'Jaguar', name: 'BOG', imageSrc: img2 },
+  { id: 3, author: 'Lil pump', name: 'Gucci Gang', imageSrc: img3 },
+  { id: 4, author: 'Kendric Lamar', name: 'Humble', imageSrc: img4 },
+  { id: 5, author: 'Zdravko Colic', name: 'Hotel', imageSrc: img5 },
+  { id: 6, author: 'Red Hot Chilly Peppers', name: 'Californication', imageSrc: img6 },
+  { id: 7, author: 'Vlado Georgiev', name: 'Hej ti', imageSrc: img7 },
+  { id: 8, author: 'Aca Lukas', name: 'Licna karta', imageSrc: img8 },
+  { id: 9, author: 'Halid Beslic', name: 'Miljacka', imageSrc: img9 },
+  { id: 10, author: 'Drake', name: 'Started From The Bottom', imageSrc: img10 },
+  { id: 11, author: 'Metallica', name: 'Enter Sandman', imageSrc: img11 },
+  { id: 12, author: 'Gorillaz', name: 'Stylo', imageSrc: img12 },
+  { id: 13, author: 'Deep Purple', name: 'Smoke on the watter', imageSrc: img13 },
+  { id: 14, author: 'Djani', name: 'Sve mi tvoje nedostaje', imageSrc: img14 },
 ];
--- src/marketplace/Marketplace.jsx
+++ src/marketplace/Marketplace.jsx
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import { getSongs } from '../getMockData';
+
+import './Marketplace.css';
 
 class Marketplace extends Component {
 
   constructor(props) {
     super(props);
-
   }
 
 
   render() {
       return (
-          <div>
-            Marketplace
+          <div className="marketplace-page-wrapper">
+            <div className="marketplace-play-wrapper">
+              Crypo Jingles
+            </div>
+
+            <div className="marketplace-wrapper">
+              <div className="sidebar">
+                All filtering options go here Lorem ipsum dolor amet Lorem ipsum dolor amet Lorem ipsum dolor amet
+                Lorem ipsum dolor amet Lorem ipsum dolor
+
+                <div>Sorting dropdown</div>
+                <div>Most popular</div>
+                <div>Genres</div>
+              </div>
+
+              <div className="songs-section">
+                <div className="songs-count">
+                  { getSongs().length } songs created
+                </div>
+
+                <div className="songs-wrapper">
+                  {
+                    getSongs().map(({ id, author, name, imageSrc }) => (
+                      <div key={id} className="single-song">
+                        <img src={ imageSrc } alt={name} />
+
+                        #{ id }
+                        <div>{ author }</div>
+                        <div>{ name }</div>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
           </div>
       )
   }
