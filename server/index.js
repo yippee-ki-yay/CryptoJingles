b1015811605c10f3bbea367257e8e0767f9fe50a
Nenad Palinkasevic <nesa993@gmail.com>
2018-01-26 20:36:21 +0100

    Listening on compose on server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 require('dotenv').load();
 
 const Web3 = require('web3');
@@ -6,15 +7,28 @@ const express = require('express');
 const db = require('./db');
 const routes = require('./routes');
 const marketplaceAbi = require("../build/contracts/Marketplace");
+const jinglesAbi = require("../build/contracts/Jingle");
 
 const app = express();
-const marketplaceAddres = "";
+const marketplaceAddress = "0x1cd2d4def506a69858f30bce024481e8fc4d3ab8";
+const jinglesAddress = "0x48961f89cd2df64766184d358bc7c2e3cb1873ba";
 
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_NODE));
 
-const marketplaceContract = web3.eth.contract(marketplaceAbi.abi).at(marketplaceAddres);
+const marketplaceContract = web3.eth.contract(marketplaceAbi.abi).at(marketplaceAddress);
+
+const jingles = web3.eth.contract(jinglesAbi.abi).at(jinglesAddress);
 
 (async () => {
+
+    jingles.Composed(async (err, res) => {
+        if(err) {
+            console.log(err);
+        }
+ 
+        console.log(res);
+    });
+
     marketplaceContract.SellOrder(async (err, res) => {
        if(err) {
            console.log(err);
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
-require('./purchase.model');
-const purchaseCtrl = require('./purchase.controller');
+require('./models/purchase.model');
+const purchaseCtrl = require('./controllers/purchase.controller');
+
+require('./models/jingles.model');
+const jingleCtrl = require('./controllers/jingles.controller');
 
 router.get('/purchase', purchaseCtrl.getPurchases);
 
