898672041211ee10fd5e5981473782954eaad5b0
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-01 20:20:36 +0100

    Style pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
 
+import './Pagination.css';
+
 // TODO - Add prototypes, style component, test with more jingles
 const Pagination = ({ pageCount, range = 10, onPageChange }) => (
-  <ReactPaginate previousLabel={"Previous"}
-                 nextLabel={"Next"}
-                 breakLabel={<a href="">...</a>}
-                 breakClassName={"break-me"}
-                 pageCount={pageCount}
-                 marginPagesDisplayed={2}
-                 pageRangeDisplayed={range}
-                 onPageChange={(data) => { onPageChange(data.selected); }}
-                 containerClassName={"pagination"}
-                 subContainerClassName={"pages pagination"}
-                 activeClassName={"active"}
-  />
+  <div className="pagination-wrapper">
+    <ReactPaginate previousLabel={"Previous"}
+                   nextLabel={"Next"}
+                   breakLabel={<a href="">...</a>}
+                   breakClassName={"break-me"}
+                   pageCount={pageCount}
+                   marginPagesDisplayed={2}
+                   pageRangeDisplayed={range}
+                   onPageChange={(data) => { onPageChange(data.selected); }}
+                   containerClassName={"pagination"}
+                   subContainerClassName={"pages pagination"}
+                   activeClassName={"active"}
+    />
+  </div>
 );
 
 export default Pagination;
