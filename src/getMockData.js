21b2df1bcdc4fb58420625bb30f8c351e59b725b
Nenad Palinkasevic <nesa993@gmail.com>
2018-02-01 15:22:42 +0100

    Hooking up ipfs

--- src/getMockData.js
+++ src/getMockData.js
@@ -21,47 +21,95 @@ import img14 from './mockImages/render_14.png';
 export const getJingleMetadata = (jingleType) => {
   switch (parseInt(jingleType)) {
     case 0:
-      return { type: COMPOSE, name: 'Bass Groove', source: '../audio/bass-groove.wav' };
+      return { type: COMPOSE, name: 'Bass Groove', source: 'https://gateway.ipfs.io/ipfs/Qmd4zeoyJGS4m1R84ygZpmghwjfm1k8JKrWu2VwLAC5cmU' };
     case 1:
-      return { type: COMPOSE, name: 'Boomin Melody', source: '../audio/boomin-melody.wav' };
+      return { type: COMPOSE, name: 'Boomin Melody', source: 'https://gateway.ipfs.io/ipfs/QmQ8JGd3TSHDKXt7voHWWKY9vZBYWh4q7W5VsjnbY8G5EH' };
     case 2:
-      return { type: COMPOSE, name: 'Brasil tropical', source: '../audio/brasil-ritmo-tropical-7.wav' };
+      return { type: COMPOSE, name: 'Brasil tropical', source: 'https://gateway.ipfs.io/ipfs/QmZau3hmNRJCywUS8mMfBne4V7mia8DxsfTKaKzrH7md71' };
     case 3:
-      return { type: COMPOSE, name: 'Bassline', source: '../audio/catchy-af-bassline-2.wav' };
+      return { type: COMPOSE, name: 'Bassline', source: 'https://gateway.ipfs.io/ipfs/QmUaax6dqv4Qw87vfk1xddSHC8DdzW6trfe7wtpuf2x9cT' };
     case 4:
-      return { type: COMPOSE, name: 'Drum beat', source: '../audio/catchy-af-drumbeat.wav' };
+      return { type: COMPOSE, name: 'Drum beat', source: 'https://gateway.ipfs.io/ipfs/QmVcSVWVBurDjB7TorMjiT1MpCQ6nYXB8QYsFwXkAPsuXy' };
     case 5:
-      return { type: COMPOSE, name: 'Synth lead', source: '../audio/catchy-af-synth-lead.wav' };
+      return { type: COMPOSE, name: 'Synth lead', source: 'https://gateway.ipfs.io/ipfs/QmahnmKgpDYjVH7Aq1XZyjfyA4joruJ3PmUDaY65o2Gjrg' };
     case 6:
-      return { type: COMPOSE, name: 'Quiet Bit', source: '../audio/cool-quiet-beat.wav' };
+      return { type: COMPOSE, name: 'Quiet Bit', source: 'https://gateway.ipfs.io/ipfs/QmS4iieauh4nJceDdaGaPNGHu6qhW6fkHD8aUrL9K2qi51' };
     case 7:
-      return { type: COMPOSE, name: 'Crystal bells', source: '../audio/crystal-bells-70-bmp.wav' };
+      return { type: COMPOSE, name: 'Crystal bells', source: 'https://gateway.ipfs.io/ipfs/Qme6qdE4V2ZgNwfKgepVuG5c1FMMtHHUsjJrKSrheVtmcQ' };
     case 8:
-      return { type: COMPOSE, name: 'Distorted drum', source: '../audio/distorted-808-drum.wav' };
+      return { type: COMPOSE, name: 'Distorted drum', source: 'https://gateway.ipfs.io/ipfs/Qmf1Tx4dUSqBDtCVmTsAAMkvhKVb3z6QugNAbSEBdWUUYj' };
     case 9:
-      return { type: COMPOSE, name: 'Drum loop', source: '../audio/drumloop-trap-with-808.wav' };
+      return { type: COMPOSE, name: 'Drum loop', source: 'https://gateway.ipfs.io/ipfs/QmRVTSXdBgUoQ5yVpeCFyc9nEzF2rcoqJmPtuwkmmGNH8Q' };
     case 10:
-      return { type: COMPOSE, name: 'Enjoy drum', source: '../audio/enjoy-drm-90bmp.wav'  };
+      return { type: COMPOSE, name: 'Enjoy drum', source: 'https://gateway.ipfs.io/ipfs/QmPpVQYBeXZpodPkrxRFFj4ZRRd7WdU8ScSrQcx9Cd8zka'  };
     case 11:
-      return { type: COMPOSE, name: 'Flair melody', source: '../audio/flair-melody-2.wav' };
+      return { type: COMPOSE, name: 'Flair melody', source: 'https://gateway.ipfs.io/ipfs/QmaDkzENCmMaAcEkShg3oS6fGownbFnu1h3T3yrjsEazMX' };
     case 12:
-      return { type: COMPOSE, name: 'Funky bass', source: '../audio/funky-bass.wav' };
+      return { type: COMPOSE, name: 'Funky bass', source: 'https://gateway.ipfs.io/ipfs/QmSRtgA7xvNMpoLBcKcLM7hDaU1B3gmBDXvo2MFHTathH8' };
     case 13:
-      return { type: COMPOSE, name: 'GG melody', source: '../audio/gg-melody.wav' };
+      return { type: COMPOSE, name: 'GG melody', source: 'https://gateway.ipfs.io/ipfs/QmXsunbyqCNueAFnF87pEWb7mojbBzraDU7Lmw3r1ubM9H' };
     case 14:
-      return { type: COMPOSE, name: 'Giving melody', source: '../audio/giving80bmp-drm.wav' };
+      return { type: COMPOSE, name: 'Giving melody', source: 'https://gateway.ipfs.io/ipfs/QmNwZbVvN4YqmSYsaUqxhbXTi74PFffB7iEk4dJ4PJreee' };
     case 15:
-      return { type: COMPOSE, name: 'Hard harp', source: '../audio/hard-harp.wav' };
+      return { type: COMPOSE, name: 'Hard harp', source: 'https://gateway.ipfs.io/ipfs/QmVcBEAEuo4646TMMWXzFhDm7KBfXdKzPJ5nV59JEXDff9' };
     case 16:
-      return { type: COMPOSE, name: 'Hi Hat', source: '../audio/hi-hat-120bpm.wav' };
+      return { type: COMPOSE, name: 'Hi Hat', source: 'https://gateway.ipfs.io/ipfs/QmcX3Ypn4sMjw1JjfNrmLn3mHLT3xZcLVPeM4zrFdBFJKW' };
     case 17:
-      return { type: COMPOSE, name: 'Huge Melody', source: '../audio/huge-melody.wav' };
+      return { type: COMPOSE, name: 'Huge Melody', source: 'https://gateway.ipfs.io/ipfs/QmQLaCEDS2yuQySPWSyXrHrE8UUFHyZnVvVqddiupcQKE4' };
     case 18:
-      return { type: COMPOSE, name: 'Hister1a', source: '../audio/hyster1a-boombap-beat.wav' };
+      return { type: COMPOSE, name: 'Hister1a', source: 'https://gateway.ipfs.io/ipfs/QmYj1Bp1LYFP84QLRVjQcnZMAPtKW8Fu5cjTRkTQPRQUvY' };
     case 19:
-      return { type: COMPOSE, name: 'I have beats', source: '../audio/i-have-132bpm.wav' };
+      return { type: COMPOSE, name: 'I have beats', source: 'https://gateway.ipfs.io/ipfs/QmUbkpETctdXnuC1FEAvwv22BkAGiojTH336Py11pDkpo4' };
     case 20:
-      return { type: COMPOSE, name: 'Love lead', source: '../audio/love-lead-v1-120bpm.wav' };
+      return { type: COMPOSE, name: 'Love lead', source: 'https://gateway.ipfs.io/ipfs/QmZQqkGZ41tSVBNnFvvLsDGjM2dERpLNguYZDtjJmSu72r' };
+    case 21:
+      return { type: COMPOSE, name: 'Piano melancholy', source: 'https://gateway.ipfs.io/ipfs/QmeMc6BAiByceNy9PRQwhXMe7PCm4jWpKeEdWUEjRAzAmw' };
+    case 22:
+      return { type: COMPOSE, name: 'Mini Bass', source: 'https://gateway.ipfs.io/ipfs/QmSJJHZvgFZa33KMSYDWFjUFmkZBa3bCAoXzGGNoq7yjuN' };
+    case 23:
+      return { type: COMPOSE, name: 'Piano loops', source: 'https://gateway.ipfs.io/ipfs/QmXdKQJLusZ5KRLfz7tt8vBXHnnpFw79yNxZHn7u1JKFpz' };
+    case 24:
+      return { type: COMPOSE, name: 'Piano Chord', source: 'https://gateway.ipfs.io/ipfs/QmcW4dG8zzjfGTp7atCuuh7F1HwrrdvvWpai1wBExeFGBW' };
+    case 25:
+      return { type: COMPOSE, name: 'Plain Jane', source: 'https://gateway.ipfs.io/ipfs/QmaB85QxjFJkdPY9wFTKp24Kc79rgAkQTVNfQyhfxBaZ2D' };
+    case 26:
+      return { type: COMPOSE, name: 'P trap', source: 'https://gateway.ipfs.io/ipfs/QmUypZyxXF1c7rR7NVzbvn1SaDtq4s6cvvn9WJ82SBv5Mv' };
+    case 27:
+      return { type: COMPOSE, name: 'Rave Synth', source: 'https://gateway.ipfs.io/ipfs/QmNcoPcsUmwN3JngxnhRc4Fi2ryKskQnTQSkXKbognFuKc' };
+    case 28:
+      return { type: COMPOSE, name: 'Robotica Synth', source: 'https://gateway.ipfs.io/ipfs/QmWr5KmpfV8mqVs5CGuHKfL7owjo7KN4oHsU8cz4QniPG2' };
+    case 29:
+      return { type: COMPOSE, name: 'Rock Guitar', source: 'https://gateway.ipfs.io/ipfs/QmVn3Pdz1sGFhBF7UTcavynawfSqgCJJrqgNXWATJSczU3' };
+    case 30:
+      return { type: COMPOSE, name: 'S1', source: 'https://gateway.ipfs.io/ipfs/QmcrZn5PHTefmqabxpzPPYPNNtK62u2wfSJGFgKaxWfsk4' };
+    case 31:
+      return { type: COMPOSE, name: 'S2', source: 'https://gateway.ipfs.io/ipfs/QmcifrAZg9gGWD7d8iMsHmeKPkRsgVVHAiZDbeQH4ApN7S' };
+    case 32:
+      return { type: COMPOSE, name: 'S3', source: 'https://gateway.ipfs.io/ipfs/QmNMFLwtXFJXNux5dGR9UwhS6sVa5YhHCGtULktdDt3YG1' };
+    case 33:
+      return { type: COMPOSE, name: 'S4', source: 'https://gateway.ipfs.io/ipfs/QmTTcHhieXixKxjw5gJp7CrEH4bYghjn5aXbqUCH8GT934' };
+    case 34:
+      return { type: COMPOSE, name: 'S5', source: 'https://gateway.ipfs.io/ipfs/QmTfdYrJ9NUf7YpUFyA2FSq3hTJNWMimbaVAmV2Y2862mf' };
+    case 35:
+      return { type: COMPOSE, name: 'S6', source: 'https://gateway.ipfs.io/ipfs/QmSwS55pCFqSWtKoSgJKirQSvT2A3Db6A7jdVz2CBK5YVL' };
+    case 36:
+      return { type: COMPOSE, name: 'S7', source: 'https://gateway.ipfs.io/ipfs/QmTX6uWP3x6r1JSNtRscKvRd2WYk9U8FMufsNRRYXSNk3R' };
+    case 37:
+      return { type: COMPOSE, name: 'Out of this', source: 'https://gateway.ipfs.io/ipfs/QmasdugxGy2uiTmUUz6j9hhMh61tqaCRXLm2offS8oMYsw' };
+    case 38:
+      return { type: COMPOSE, name: 'Climax 170', source: 'https://gateway.ipfs.io/ipfs/QmYMYbXfEtXutqigurQ3Xq8s1ogjdT22KBBmiHcBbo9mnK' };
+    case 39:
+      return { type: COMPOSE, name: 'Keys', source: 'https://gateway.ipfs.io/ipfs/QmQ9TWUQyEj51P9FeanwkkFq2eNFsNtudcaowuBrWwCnfh' };
+    case 40:
+      return { type: COMPOSE, name: 'Slide', source: 'https://gateway.ipfs.io/ipfs/QmVP6oxnsaE3qauBLM5SjjELrASehmwcpEJQAbx5E9qoyV' };
+    case 41:
+      return { type: COMPOSE, name: 'Tip Tap', source: 'https://gateway.ipfs.io/ipfs/QmQdEFdaZgs6XRU9k3MrcUKZFfhSYCPrYCVwE31aepm46J' };
+    case 42:
+      return { type: COMPOSE, name: 'Spooky', source: 'https://gateway.ipfs.io/ipfs/QmTudsxzbp79m8QJJoDYHdYfbJCCsdgv443dasJQZANoFj' };
+    case 43:
+      return { type: COMPOSE, name: 'The lies', source: 'https://gateway.ipfs.io/ipfs/QmTK31acPf8rcshhCbmEHqFMQZ1jPha3MLDWPvvecfoaSa' };
+    case 44:
+      return { type: COMPOSE, name: 'Backup lies', source: 'https://gateway.ipfs.io/ipfs/QmPr7YCP7rGpn8iaefoueRR9V1FHTxHXme2FPrznW3pDkX' };
     default:
       return { type: COMPOSE, name: '', source: '' };
   }
