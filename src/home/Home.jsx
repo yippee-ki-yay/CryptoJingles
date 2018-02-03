1131415c0776dd7d35cefb46518e7f1fafcd44c5
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-03 22:14:54 +0100

    Edit homepage text, style footer

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -13,10 +13,10 @@ class Home extends Component {
     super(props);
 
     this.state = {
-      sample1: getJingleMetadata(24),
-      sample2: getJingleMetadata(67),
-      sample3: getJingleMetadata(90),
-      sample4: getJingleMetadata(0),
+      sample1: { jingleType: 24, ...getJingleMetadata(24) },
+      sample2: { jingleType: 67, ...getJingleMetadata(67) },
+      sample3: { jingleType: 90, ...getJingleMetadata(90) },
+      sample4: { jingleType: 0, ...getJingleMetadata(0) },
       jingle: {
         author: 'Soundtoshi Nakajingles',
         jingleId: 7,
@@ -71,7 +71,7 @@ class Home extends Component {
               future. A sample can be one of 4 types of rarity: common, rare, legendary and mythical. Rarer the sample,
               better the sound. There are currently 59 common sounds, 30 rare, 10 legendary and only 1 mythical!
               Every sound sample is hosted on IPFS and referenced on the main smart contract. Samples have a fixed
-              price of 0.001Ξ and can't be traded. In order to compose a jingle you need at least 5
+              price of 0.001Ξ and can't be traded. In order to compose a jingle you need 5
               samples. When a sample is used to compose a jingle it becomes a part of it and you can't use it in
               other jingles.
             </div>
@@ -96,24 +96,29 @@ class Home extends Component {
             </div>
           </div>
 
-          <div className="description">
+          <div className="description description-jingle">
             <h3>Jingles</h3>
-            <div className="sample-text">
-            Every jingle is unique both by it’s sound but also with a generated image specific for that jingle.
-            When you create a jingle nobody else can create that same jingle.
-            Jingles are tradable items you can put them on sale on our Marketplace for whatever
-            price you think it’s worth. You can put them on sale for free but we do take a 3% cut on a successful sale.
-            Every jingle also has an author and a name, which users are free to set and it’s saved on the blockchain.
+            <div className="sample-text sample-text-jingle">
+              Every jingle is unique in two aspects. One, nobody can create a jingle with the same combination of
+              sound samples. Two, we generate a unique cover image that is based on the jingle identificator. As a
+              result, a jingle can have value because of its cover image and its sound. Jingles are tradable items
+              that you can put on sale in our marketplace for whatever price you think it’s worth.
+              Putting a jingle on sale is free, but we do take a 3% cut on a successful sale. Additionally, a jingle
+              has a name and a author. They are set when a jingle is composed and can't be altered after that, even
+              after a change of ownership. The default name for every owner is Soundtoshi Nakajingles, you are free to
+              change this on your profile page.
             </div>
           </div>
         </div>
 
-        <div className="separator" />
+        <div className="separator last-separator" />
 
-        <div>
-          <a href="https://www.reddit.com/r/CryptoJingles/">Reddit</a>
-          <a href="https://www.reddit.com/r/CryptoJingles/">Medium</a>
-          <a href="https://discord.gg/F6PW2Tx">Discord</a>
+        <div className="home-footer">
+          Fell free to ask us anything on
+          <a className="reddit" href="https://www.reddit.com/r/CryptoJingles/"  target="_blank" rel="noopener">Reddit</a>,
+          <a className="medium" href="https://medium.com/@cryptojingles"  target="_blank" rel="noopener">Medium</a> or
+          <a className="discord" href="https://discord.gg/F6PW2Tx"  target="_blank" rel="noopener">Discord</a>,
+          we will gladly answer all questions.
         </div>
       </div>
     )
