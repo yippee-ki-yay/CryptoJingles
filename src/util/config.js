cf105d01d6b30cb0373bbfd06135e972b181f81a
Nenad Palinkasevic <nesa993@gmail.com>
2018-02-10 21:04:02 +0100

    Contract change

--- src/util/config.js
+++ src/util/config.js
@@ -1,10 +1,10 @@
 // On kovan
 
-export const CryptoJinglesAddress = '0xb956c4f01c4d52be118bdddfe440a533e73281d1';
-export const JingleAddress = '0x9430a9881ded68c90471dd2304b1073aba088f59';
-export const SampleAddress = '0xed2c7710eed3ea04ff6b5210561ddacb79c4065d';
-export const SampleStorage = '0x96390beb1fc488c0c586964c66e36b4529f74d07';
-export const MarketplaceAddress = '0x31337c5b1580d8c5fa6880ea34e323364165ed17';
+export const CryptoJinglesAddress = '0x395673c2fa0df574b68750410777b69d7fcdf0e6';
+export const JingleAddress = '0x91d485e988ce138570a02f23c4527872e1f6cb21';
+export const SampleAddress = '0xb3f3233a49a1c54cfc06db5a034a2601e14a3d58';
+export const SampleStorage = '0x96390beb1fc488c0c586964c66e36b4529f74d07';		 
+export const MarketplaceAddress = '0x110949757c7443683389be27402b8e5e26c18fd4';
 
 export const SAMPLE_PRICE = 1000000000000000;
 export const API_URL = 'http://localhost:9999/api';
