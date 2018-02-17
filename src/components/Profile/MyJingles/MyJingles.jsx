641a5af013085b30de7b931e3cf65dd8b081db4b
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 20:34:01 +0100

    Fix eslint errors for MyJingles component

--- src/components/Profile/MyJingles/MyJingles.jsx
+++ src/components/Profile/MyJingles/MyJingles.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import BoxLoader from '../../Decorative/BoxLoader';
 import Pagination from '../../Pagination/Pagination';
 import SingleJingle from '../../SingleJingle/SingleJingle';
 import {
-  getJinglesForUser, changeProfileJinglesCategory, changeProfileJinglesSorting, onMyJinglesPaginationChange
+  getJinglesForUser, changeProfileJinglesCategory, changeProfileJinglesSorting, onMyJinglesPaginationChange,
 } from '../../../actions/profileActions';
 import { MARKETPLACE_JINGLES_PER_PAGE } from '../../../constants/actionTypes';
 
@@ -23,46 +24,46 @@ class MyJingles extends Component {
   }
 
   render() {
-      const {
-        myJingles, loading, jingleSorting, jingleSortingOptions, jingleCategory, jingleCategories, totalJingles,
-        jinglesPerPage
-      } = this.props;
-      const { changeProfileJinglesSorting, changeProfileJinglesCategory, onMyJinglesPaginationChange } = this.props;
+    const {
+      myJingles, loading, jingleSorting, jingleSortingOptions, jingleCategory, jingleCategories, totalJingles,
+      jinglesPerPage,
+    } = this.props;
+    const { changeProfileJinglesSorting, changeProfileJinglesCategory, onMyJinglesPaginationChange } = this.props;
 
-      return (
-          <div className="my-jingles-wrapper">
-            <div className="samples-wrapper">
-              <div>
-                <div className="my-jingles-sorting-wrapper">
-                  <div className="sort-wrapper">
-                    <div className="sort-wrapper-label">Category:</div>
-                    <Dropdown
-                      onChange={(val) => { changeProfileJinglesCategory(val) }}
-                      value={jingleCategory}
-                      options={jingleCategories}
-                    />
-                  </div>
-
-                  <div className="sort-wrapper">
-                    <div className="sort-wrapper-label">Sort by:</div>
-                    <Dropdown
-                      onChange={(val) => { changeProfileJinglesSorting(val) }}
-                      value={jingleSorting}
-                      options={jingleSortingOptions}
-                    />
-                  </div>
-                </div>
-                <div className="separator" />
+    return (
+      <div className="my-jingles-wrapper">
+        <div className="samples-wrapper">
+          <div>
+            <div className="my-jingles-sorting-wrapper">
+              <div className="sort-wrapper">
+                <div className="sort-wrapper-label">Category:</div>
+                <Dropdown
+                  onChange={(val) => { changeProfileJinglesCategory(val); }}
+                  value={jingleCategory}
+                  options={jingleCategories}
+                />
               </div>
 
-              {
+              <div className="sort-wrapper">
+                <div className="sort-wrapper-label">Sort by:</div>
+                <Dropdown
+                  onChange={(val) => { changeProfileJinglesSorting(val); }}
+                  value={jingleSorting}
+                  options={jingleSortingOptions}
+                />
+              </div>
+            </div>
+            <div className="separator" />
+          </div>
+
+          {
                 loading &&
                 <div className="loader-wrapper">
                   <BoxLoader />
                 </div>
               }
 
-              {
+          {
                 (myJingles.length === 0) &&
                 !loading &&
                 <div className="empty-state">
@@ -70,7 +71,7 @@ class MyJingles extends Component {
                 </div>
               }
 
-              {
+          {
                 (myJingles.length > 0) &&
                 !loading &&
                 <div>
@@ -81,30 +82,45 @@ class MyJingles extends Component {
                   <div className="my-jingles-list">
 
                     {
-                      myJingles.map((jingle) =>
+                      myJingles.map(jingle =>
                         (
                           <SingleJingle key={jingle.jingleId} {...jingle} type="profile" />
-                        )
-                      )
+                        ))
                     }
                   </div>
                 </div>
               }
 
-              {
+          {
                 totalJingles > MARKETPLACE_JINGLES_PER_PAGE &&
                 <Pagination
                   pageCount={Math.ceil(totalJingles / jinglesPerPage)}
                   onPageChange={onMyJinglesPaginationChange}
                 />
               }
-            </div>
-          </div>
-      )
+        </div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+MyJingles.propTypes = {
+  address: PropTypes.string.isRequired,
+  myJingles: PropTypes.array.isRequired,
+  totalJingles: PropTypes.number.isRequired,
+  jinglesPerPage: PropTypes.number.isRequired,
+  loading: PropTypes.bool.isRequired,
+  jingleSorting: PropTypes.object.isRequired,
+  jingleSortingOptions: PropTypes.array.isRequired,
+  jingleCategory: PropTypes.object.isRequired,
+  jingleCategories: PropTypes.array.isRequired,
+  getJinglesForUser: PropTypes.func.isRequired,
+  changeProfileJinglesCategory: PropTypes.func.isRequired,
+  changeProfileJinglesSorting: PropTypes.func.isRequired,
+  onMyJinglesPaginationChange: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
   myJingles: state.profile.myJingles,
   totalJingles: state.profile.totalJingles,
   jinglesPerPage: state.profile.jinglesPerPage,
@@ -112,11 +128,11 @@ const mapStateToProps = (state) => ({
   jingleSorting: state.profile.jingleSorting,
   jingleSortingOptions: state.profile.jingleSortingOptions,
   jingleCategory: state.profile.jingleCategory,
-  jingleCategories: state.profile.jingleCategories
+  jingleCategories: state.profile.jingleCategories,
 });
 
 const mapDispatchToProps = {
-  getJinglesForUser, changeProfileJinglesCategory, changeProfileJinglesSorting, onMyJinglesPaginationChange
+  getJinglesForUser, changeProfileJinglesCategory, changeProfileJinglesSorting, onMyJinglesPaginationChange,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyJingles);
