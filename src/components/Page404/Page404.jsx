bc9aa4a6148d9f64e868453b708d916b95b2dae2
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 19:41:49 +0100

    Fix eslint errors for Page404

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -7,7 +7,7 @@ const MyAlbums = () => (
   <div className="container page404-wrapper">
     <h1>MIC CHECK.</h1>
     <h1>MIC CHECK.</h1>
-    <h5>Uh oh, you're lost. The page you are looking for doesn't exits.</h5>
+    <h5>Uh oh, you{'\''}re lost. The page you are looking for doesn{'\''}t exits.</h5>
 
     <Link to="/">RETURN HOME</Link>
   </div>
