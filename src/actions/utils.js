053c2eb2ed9c56c894c38e9a63c224f5b5fda69e
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 21:42:21 +0100

    Fix eslint errors for utils

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -5,13 +5,13 @@ import { API_URL } from '../util/config';
  * Converts price in wei to price in ETH without web3
  * @param price - ether
  */
-export const formatSalePrice = (price) => (String(parseFloat(price) / 1000000000000000000)).slice(0, 8);
+export const formatSalePrice = price => (String(parseFloat(price) / 1000000000000000000)).slice(0, 8);
 
 /**
  * Converts price in ethers to price in wei without web3
  * @param price - wei
  */
-export const formatToWei = (price) => (parseFloat(price) * 1000000000000000000);
+export const formatToWei = price => (parseFloat(price) * 1000000000000000000);
 
 /**
  * Prompts user to sign message in order to
