6444b039fdc6261e1ad29c2acef93ccd1ef91161
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 19:45:49 +0100

    Fix eslint errors for Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
+import PropTypes from 'prop-types';
 
 import './Pagination.scss';
 
-// TODO - Add prototypes, style component, test with more jingles
-const Pagination = ({ pageCount, range = 10, onPageChange }) => (
+const Pagination = ({ pageCount, onPageChange, range }) => (
   <div className="pagination-wrapper">
-    <ReactPaginate previousLabel={"Previous"}
-                   nextLabel={"Next"}
-                   breakLabel={<a href="">...</a>}
-                   breakClassName={"break-me"}
-                   pageCount={pageCount}
-                   marginPagesDisplayed={2}
-                   pageRangeDisplayed={range}
-                   onPageChange={(data) => { onPageChange(data.selected); }}
-                   containerClassName={"pagination"}
-                   subContainerClassName={"pages pagination"}
-                   activeClassName={"active"}
+    <ReactPaginate
+      previousLabel="Previous"
+      nextLabel="Next"
+      breakLabel={<a href="">...</a>}
+      breakClassName="break-me"
+      pageCount={pageCount}
+      marginPagesDisplayed={2}
+      pageRangeDisplayed={range}
+      onPageChange={(data) => { onPageChange(data.selected); }}
+      containerClassName="pagination"
+      subContainerClassName="pages pagination"
+      activeClassName="active"
     />
   </div>
 );
+
+Pagination.defaultProps = {
+  range: 10,
+};
+
+Pagination.propTypes = {
+  pageCount: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  range: PropTypes.number,
+};
 
 export default Pagination;
