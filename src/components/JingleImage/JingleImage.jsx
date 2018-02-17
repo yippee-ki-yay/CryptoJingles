ed4de66dc4eeedb1cec0ab7c6306c7714a9e2bf4
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 13:31:05 +0100

    Fix lint errors for Jingle image component, fix errors in Logo component

--- src/components/JingleImage/JingleImage.jsx
+++ src/components/JingleImage/JingleImage.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import createLoop from 'raf-loop';
+import PropTypes from 'prop-types';
 import createConfig from '../../../jingleImageGeneration/createConfigClient';
-import createRenderer  from '../../../jingleImageGeneration/createRenderer';
+import createRenderer from '../../../jingleImageGeneration/createRenderer';
 
 import './JingleImage.scss';
 
-const letterbox = (element, parent) => {
-  let aspect = element.width / element.height;
-  let pwidth = parent.offsetWidth;
-  let pheight = parent.offsetHeight;
+// Uncomment this if you want to resize the canvas
+// const letterbox = (element, parent) => {
+//   let aspect = element.width / element.height;
+//   let pwidth = parent.offsetWidth;
+//   let pheight = parent.offsetHeight;
+//
+//   let width = pwidth;
+//   let height = Math.round(width / aspect);
+//   let y = Math.floor(pheight - height) / 2;
+//
+//   element.style.top = y + 'px';
+//   element.style.width = width + 'px';
+//   element.style.height = height + 'px';
+// };
 
-  let width = pwidth;
-  let height = Math.round(width / aspect);
-  let y = Math.floor(pheight - height) / 2;
-
-  element.style.top = y + 'px';
-  element.style.width = width + 'px';
-  element.style.height = height + 'px';
-};
-
-// TODO - add proptypes
 class JingleImage extends Component {
   constructor(props) {
     super(props);
@@ -34,46 +35,46 @@ class JingleImage extends Component {
     // document.addEventListener('resize', this.resize.bind(this));
   }
 
-  componentWillUnmount() {
-    // document.addEventListener('resize', this.resize.bind(this));
-  }
+  // componentWillUnmount() {
+  //   document.addEventListener('resize', this.resize.bind(this));
+  // }
 
-  resize() {
-    letterbox(this.canvas, this.canvasWrapper);
-  }
+  // resize() {
+  //   letterbox(this.canvas, this.canvasWrapper);
+  // }
 
   reload(config) {
-    let canvas = this.canvas;
-    let background = new window.Image();
-    let context = canvas.getContext('2d');
-    let loop = createLoop();
+    const { canvas } = this;
+    const background = new window.Image();
+    const context = canvas.getContext('2d');
+    const loop = createLoop();
 
     loop.removeAllListeners('tick');
     loop.stop();
 
-    let opts = {
+    const opts = {
       ...config,
       backgroundImage: background,
-      context: context,
+      context,
       width: this.props.width,
       height: this.props.height,
     };
 
-    let pixelRatio = typeof opts.pixelRatio === 'number' ? opts.pixelRatio : 1;
+    const pixelRatio = typeof opts.pixelRatio === 'number' ? opts.pixelRatio : 1;
     canvas.width = opts.width * pixelRatio;
     canvas.height = opts.height * pixelRatio;
 
     this.canvasWrapper.style.background = opts.palette[0];
 
     background.onload = () => {
-      let renderer = createRenderer(opts);
+      const renderer = createRenderer(opts);
       renderer.clear();
 
       let stepCount = 0;
 
       loop.on('tick', () => {
         renderer.step(opts.interval);
-        stepCount++;
+        stepCount += 1;
         if (!opts.endlessBrowser && stepCount > opts.steps) loop.stop();
       });
 
@@ -85,12 +86,18 @@ class JingleImage extends Component {
 
   render() {
     return (
-      <div className="jingle-image-wrapper" ref={(ref) => this.canvasWrapper = ref}>
-        <canvas ref={(ref) => this.canvas = ref} />
+      <div className="jingle-image-wrapper" ref={(ref) => { this.canvasWrapper = ref; }}>
+        <canvas ref={(ref) => { this.canvas = ref; }} />
       </div>
-    )
+    );
   }
 }
+
+JingleImage.propTypes = {
+  id: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+};
 
 export default JingleImage;
 
