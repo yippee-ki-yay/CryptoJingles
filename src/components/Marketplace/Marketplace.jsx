2fb603bc8b33c5cf0cb2e891ec8e038bf0cfe35d
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 18:06:20 +0100

    Fix eslint errors for Marketplace

--- src/components/Marketplace/Marketplace.jsx
+++ src/components/Marketplace/Marketplace.jsx
@@ -2,15 +2,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import PropTypes from 'prop-types';
 import SingleJingle from '../SingleJingle/SingleJingle';
 import Pagination from '../Pagination/Pagination';
 import {
   getMarketplaceJingles, changeMarketplaceCategory, changeMarketplaceSorting,
-  onMarketplacePaginationChange
+  onMarketplacePaginationChange,
 } from '../../actions/marketplaceActions';
 import { MARKETPLACE_JINGLES_PER_PAGE } from '../../constants/actionTypes';
 
-import 'react-dropdown/style.css';
 import './Marketplace.scss';
 
 // TODO - Add prototypes
@@ -20,76 +21,93 @@ class Marketplace extends Component {
   }
 
   render() {
-    const { jingles, jinglesPerPage, sorting, sortingOptions, category, categories, totalJingles } = this.props;
+    const {
+      jingles, jinglesPerPage, sorting, sortingOptions, category, categories, totalJingles,
+    } = this.props;
     const { changeMarketplaceCategory, changeMarketplaceSorting, onMarketplacePaginationChange } = this.props;
 
-      return (
-          <div className="marketplace-page-wrapper container">
-            <div className="marketplace-wrapper">
+    return (
+      <div className="marketplace-page-wrapper container">
+        <div className="marketplace-wrapper">
 
-              { /* SIDEBAR RENDER (TODO - Create component) */ }
-              <div className="sidebar">
+          { /* SIDEBAR RENDER (TODO - Create component) */ }
+          <div className="sidebar">
 
-                <div className="sort-wrapper">
-                  <div className="sort-wrapper-label">Category:</div>
-                  <Dropdown
-                    onChange={(val) => { changeMarketplaceCategory(val) }}
-                    value={category}
-                    options={categories}
-                  />
-                </div>
+            <div className="sort-wrapper">
+              <div className="sort-wrapper-label">Category:</div>
+              <Dropdown
+                onChange={(val) => { changeMarketplaceCategory(val); }}
+                value={category}
+                options={categories}
+              />
+            </div>
 
-                <div className="sort-wrapper">
-                  <div className="sort-wrapper-label">Sort by:</div>
-                  <Dropdown
-                    onChange={(val) => { changeMarketplaceSorting(val) }}
-                    value={sorting}
-                    options={sortingOptions}
-                  />
-                </div>
+            <div className="sort-wrapper">
+              <div className="sort-wrapper-label">Sort by:</div>
+              <Dropdown
+                onChange={(val) => { changeMarketplaceSorting(val); }}
+                value={sorting}
+                options={sortingOptions}
+              />
+            </div>
 
-              </div>
+          </div>
 
-              { /* JINGLES RENDER (TODO - Create component) */ }
-              <div className="songs-section">
-                <div className="songs-count">
-                  { totalJingles } Jingles
-                </div>
+          { /* JINGLES RENDER (TODO - Create component) */ }
+          <div className="songs-section">
+            <div className="songs-count">
+              { totalJingles } Jingles
+            </div>
 
-                <div className="songs-wrapper">
-                  { jingles.map((jingle) => (
-                    <SingleJingle
-                      type="marketplace"
-                      key={jingle.jingleId} {...jingle}
-                    />)) }
-                </div>
+            <div className="songs-wrapper">
+              { jingles.map(jingle => (
+                <SingleJingle
+                  type="marketplace"
+                  key={jingle.jingleId}
+                  {...jingle}
+                />)) }
+            </div>
 
-                {
+            {
                   totalJingles > MARKETPLACE_JINGLES_PER_PAGE &&
                   <Pagination
                     pageCount={Math.ceil(totalJingles / jinglesPerPage)}
                     onPageChange={onMarketplacePaginationChange}
                   />
                 }
-              </div>
-            </div>
           </div>
-      )
+        </div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+Marketplace.propTypes = {
+  jingles: PropTypes.array.isRequired,
+  jinglesPerPage: PropTypes.number.isRequired,
+  sorting: PropTypes.object.isRequired,
+  category: PropTypes.object.isRequired,
+  categories: PropTypes.array.isRequired,
+  totalJingles: PropTypes.number.isRequired,
+  sortingOptions: PropTypes.array.isRequired,
+  changeMarketplaceCategory: PropTypes.func.isRequired,
+  changeMarketplaceSorting: PropTypes.func.isRequired,
+  getMarketplaceJingles: PropTypes.func.isRequired,
+  onMarketplacePaginationChange: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
   totalJingles: state.marketplace.totalJingles,
   jinglesPerPage: state.marketplace.jinglesPerPage,
   jingles: state.marketplace.jingles,
   sorting: state.marketplace.sorting,
   sortingOptions: state.marketplace.sortingOptions,
   category: state.marketplace.category,
-  categories: state.marketplace.categories
+  categories: state.marketplace.categories,
 });
 
 const mapDispatchToProps = {
-  getMarketplaceJingles, changeMarketplaceCategory, changeMarketplaceSorting, onMarketplacePaginationChange
+  getMarketplaceJingles, changeMarketplaceCategory, changeMarketplaceSorting, onMarketplacePaginationChange,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Marketplace);
