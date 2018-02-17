5360e9ef3b96464ca3486643e1fc224a163b4320
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 21:34:26 +0100

    Fix eslint errors for audioActions

--- src/actions/audioActions.js
+++ src/actions/audioActions.js
@@ -1,6 +1,6 @@
 import { SET_SINGLE_AUDIO } from '../constants/actionTypes';
 
-export const playAudio = (payload) => (dispatch) => {
+export const playAudio = payload => (dispatch) => {
   dispatch({ type: SET_SINGLE_AUDIO, payload });
 };
 
