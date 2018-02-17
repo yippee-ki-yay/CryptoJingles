cce5a9dbd81f17f4c8cda76a728dd016eb411cd8
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 13:20:42 +0100

    Fix lint errors for Decorative components

--- src/components/Decorative/LoadingIcon.jsx
+++ src/components/Decorative/LoadingIcon.jsx
@@ -5,7 +5,7 @@ import './LoadingIcon.scss';
 const PlayIcon = () => (
   <div className="loading-icon-wrapper">
     <svg id="load" x="0px" y="0px" viewBox="0 0 150 150">
-      <circle id="loading-inner" cx="75" cy="75" r="60"/>
+      <circle id="loading-inner" cx="75" cy="75" r="60" />
     </svg>
   </div>
 );
