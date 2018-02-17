94d98a15b5d25a832502d94addf9d639ba6c53f3
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-17 21:36:11 +0100

    Fix eslint errors for marketplaceActions

--- src/actions/marketplaceActions.js
+++ src/actions/marketplaceActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '../util/config';
 import {
   SET_MARKETPLACE_CATEGORY, SET_MARKETPLACE_JINGLES, SET_MARKETPLACE_SORT, SET_MARKETPLACE_PAGE,
-  MARKETPLACE_LIKE_UNLIKE_JINGLE
+  MARKETPLACE_LIKE_UNLIKE_JINGLE,
 } from '../constants/actionTypes';
 import { likeUnlikeJingle } from '../actions/utils';
 
@@ -12,7 +12,7 @@ import { likeUnlikeJingle } from '../actions/utils';
  * @return {Function}
  */
 export const getMarketplaceJingles = () => async (dispatch, getState) => {
-  const { currentPage, category, sorting, } = getState().marketplace;
+  const { currentPage, category, sorting } = getState().marketplace;
   const { hasMM, lockedMM, address } = getState().app;
   let jingles = [];
 
@@ -25,20 +25,21 @@ export const getMarketplaceJingles = () => async (dispatch, getState) => {
     if ((hasMM && !lockedMM) && jingleIds.length > 0) {
       const likedJinglesResponse = await axios(`${API_URL}/jingles/check-liked/${address}/${jingleIds}`);
       jingles = response.data.map((_jingle, index) => ({
-        ..._jingle, liked: likedJinglesResponse.data[index]
+        ..._jingle, liked: likedJinglesResponse.data[index],
       }));
     }
 
     if ((!hasMM || lockedMM) && jingleIds.length > 0) {
-      jingles = response.data.map((_jingle, index) => ({ ..._jingle, liked: false }));
+      jingles = response.data.map(_jingle => ({ ..._jingle, liked: false }));
     }
 
     if (jingleIds.length === 0) jingles = response.data;
 
     // false for all jingles, true to get jingles on sale
-    const num = await axios(`${API_URL}/jingles/count/filter/${sorting.value}/sale/${(category.value === 'sale').toString()}`);
+    const query = `${API_URL}/jingles/count/filter/${sorting.value}/sale/${(category.value === 'sale').toString()}`;
+    const num = await axios(query);
 
-    dispatch({ type: SET_MARKETPLACE_JINGLES, payload: { jingles, num: num.data } })
+    dispatch({ type: SET_MARKETPLACE_JINGLES, payload: { jingles, num: num.data } });
   } catch (err) {
     // console.error('Get marketplace jingles error:', err); // TODO Handle this in the future
   }
@@ -52,7 +53,7 @@ export const getMarketplaceJingles = () => async (dispatch, getState) => {
  *
  * @return {Function}
  */
-export const changeMarketplaceCategory = (payload) => (dispatch) => {
+export const changeMarketplaceCategory = payload => (dispatch) => {
   dispatch({ type: SET_MARKETPLACE_CATEGORY, payload });
   dispatch(getMarketplaceJingles());
 };
@@ -65,7 +66,7 @@ export const changeMarketplaceCategory = (payload) => (dispatch) => {
  *
  * @return {Function}
  */
-export const changeMarketplaceSorting = (payload) => (dispatch) => {
+export const changeMarketplaceSorting = payload => (dispatch) => {
   dispatch({ type: SET_MARKETPLACE_SORT, payload });
   dispatch(getMarketplaceJingles());
 };
@@ -77,7 +78,7 @@ export const changeMarketplaceSorting = (payload) => (dispatch) => {
  *
  * @return {Function}
  */
-export const onMarketplacePaginationChange = (pageNum) => (dispatch) => {
+export const onMarketplacePaginationChange = pageNum => (dispatch) => {
   dispatch({ type: SET_MARKETPLACE_PAGE, payload: pageNum + 1 });
   dispatch(getMarketplaceJingles());
 };
@@ -91,20 +92,20 @@ export const onMarketplacePaginationChange = (pageNum) => (dispatch) => {
  * @return {Function}
  */
 export const likeUnLikeMarketplaceJingle = (jingleId, action) => async (dispatch, getState) => {
-    const state = getState();
-    const { address } = state.app;
+  const state = getState();
+  const { address } = state.app;
 
-    try {
-      const likeData = await likeUnlikeJingle(jingleId, action, address);
-      if (!likeData) return;
+  try {
+    const likeData = await likeUnlikeJingle(jingleId, action, address);
+    if (!likeData) return;
 
-      const jingles = [...state.marketplace.jingles];
-      const jingleIndex = jingles.findIndex(_jingle => _jingle.jingleId === jingleId);
+    const jingles = [...state.marketplace.jingles];
+    const jingleIndex = jingles.findIndex(_jingle => _jingle.jingleId === jingleId);
 
-      jingles[jingleIndex] = { ...jingles[jingleIndex], ...likeData };
-      dispatch({ type: MARKETPLACE_LIKE_UNLIKE_JINGLE, payload: jingles });
-    } catch (err) {
-      // TODO Handle this in the future
-    }
+    jingles[jingleIndex] = { ...jingles[jingleIndex], ...likeData };
+    dispatch({ type: MARKETPLACE_LIKE_UNLIKE_JINGLE, payload: jingles });
+  } catch (err) {
+    // TODO Handle this in the future
+  }
 };
 
