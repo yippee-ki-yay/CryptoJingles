8733f79927f9978a0a36a9ebb17b80f73bce2652
astankovic <aleksa.stankovic@berkeley.edu>
2018-02-18 13:56:14 +0100

    Fix lint errors for sound helper

--- src/util/soundHelper.js
+++ src/util/soundHelper.js
@@ -1,64 +1,63 @@
-export const playWithDelay = (group, settings, samplesPos) => {
-    let delays = settings.slice(5, 11);
-    let startCuts = settings.slice(10, 16);
-    let endCuts = settings.slice(15, 21);
+const defaultSamplePos = [
+  { lastDroppedItem: true },
+  { lastDroppedItem: true },
+  { lastDroppedItem: true },
+  { lastDroppedItem: true },
+  { lastDroppedItem: true },
+];
 
-    if (!samplesPos) {
-        samplesPos = [
-            { lastDroppedItem: true },
-            { lastDroppedItem: true },
-            { lastDroppedItem: true },
-            { lastDroppedItem: true },
-            { lastDroppedItem: true },
-        ];
+export const playWithDelay = (group, settings, samplesPos = defaultSamplePos) => {
+  let delays = settings.slice(5, 11);
+  let startCuts = settings.slice(10, 16);
+  let endCuts = settings.slice(15, 21);
+
+  delays = delays.map(d => parseInt(d, 10) / 10);
+  startCuts = startCuts.map(d => parseInt(d, 10) / 10);
+  endCuts = endCuts.map(d => parseInt(d, 10) / 10);
+
+  let longest = 0;
+
+  let soundIndex = 0;
+  let longestSound = null;
+
+  for (let i = 0; i < 5; i += 1) {
+    if (samplesPos[i].lastDroppedItem) {
+      const sound = group.sounds[soundIndex];
+
+      sound.play(delays[i], startCuts[i]);
+
+      const length = sound.getRawSourceNode().buffer.duration;
+
+      let whenToStop = length + delays[i];
+
+      if (startCuts[i] !== 0) {
+        whenToStop = (endCuts[i] - startCuts[i]) + delays[i];
+      } else if (endCuts[i] !== 0) {
+        whenToStop = ((length) - (length - endCuts[i])) + delays[i];
+      }
+
+      if (whenToStop > longest) {
+        longest = whenToStop;
+        longestSound = sound;
+      }
+
+      setTimeout(() => {
+        sound.stop();
+      }, whenToStop * 1000);
+
+      soundIndex += 1;
     }
-
-    delays = delays.map(d => parseInt(d) / 10);
-    startCuts = startCuts.map(d => parseInt(d) / 10);
-    endCuts = endCuts.map(d => parseInt(d) / 10);
-
-    let longest = 0;
-
-    let soundIndex = 0;
-    let longestSound = null;
-
-    for(let i = 0; i < 5; ++i) {
-        if (samplesPos[i].lastDroppedItem) {
-            const sound = group.sounds[soundIndex];
-
-            sound.play(delays[i], startCuts[i]);
-
-            const length = sound.getRawSourceNode().buffer.duration;
-
-            let whenToStop = length + delays[i];
-
-            if (startCuts[i] !== 0) {
-                whenToStop = (endCuts[i] - startCuts[i]) + delays[i];
-            } else if (endCuts[i] !== 0) {
-                whenToStop = ((length) - (length - endCuts[i])) + delays[i];
-            }
-
-            if (whenToStop > longest) {
-                longest = whenToStop;
-                longestSound = sound;
-            }
-            
-            setTimeout(() => {
-                sound.stop();
-            }, whenToStop * 1000);
-
-            soundIndex++;
-        }
-    }
-
-    return longestSound;
   }
 
-  export const createSettings = (props) => {
-    let { volumes, delays, cuts } = props;
+  return longestSound;
+};
 
-    delays = delays.map(d => d * 10);
-    cuts = cuts.map(c => c * 10);
+export const createSettings = (props) => {
+  let delays = [...props.delays];
+  let cuts = [...props.cuts];
 
-    return [...volumes, ...delays, ...cuts];
-  }
+  delays = delays.map(d => d * 10);
+  cuts = cuts.map(c => c * 10);
+
+  return [...delays, ...cuts];
+};
