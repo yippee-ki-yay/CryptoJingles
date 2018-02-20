fe1fcc5443dc7cce7f5d1c1d7f6d513d8d4e5837
Nenad Palinkasevic <nesa993@gmail.com>
2018-02-20 14:50:03 +0100

    changed api url

--- src/util/web3/getWeb3.js
+++ src/util/web3/getWeb3.js
@@ -21,8 +21,8 @@ const getWeb3 = async (dispatch) => {
     window.web3 = new Web3(web3.currentProvider); // eslint-disable-line
 
     // TODO - check if dev or prod here
-    const netId = await window.web3.eth.net.getId();
-    if (netId !== '1') alert('Wrong network, please switch to the mainnet!');
+    // const netId = await window.web3.eth.net.getId();
+    // if (netId !== '1') alert('Wrong network, please switch to the mainnet!');
 
     // Init jingles contract
     const cryptoJinglesContract = contract(CryptoJingles);
