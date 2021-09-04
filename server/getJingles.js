befb7ad9cf4528b85e8aa3442015c273ef2fc1f2
Nenad Palinkasevic <nesa993@gmail.com>
2021-09-04 10:09:18 +0200

    backfill fix

--- server/getJingles.js
+++ server/getJingles.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-lonely-if */
+/* eslint-disable no-restricted-syntax */
 require('dotenv').config();
 
 const Web3 = require('web3');
@@ -7,73 +9,92 @@ const web3 = new Web3(process.env.NODE_URL);
 const db = require('./db');
 const routes = require('./routes');
 
-const marketplaceAbi = require("./abis/Marketplace.json");
-const cryptoJinglesAbi = require("./abis/CryptoJingle");
-const jinglesAbi = require("./abis/Jingle");
+const marketplaceAbi = require('./abis/Marketplace.json');
+const cryptoJinglesAbi = require('./abis/CryptoJingle');
+const jinglesAbi = require('./abis/Jingle');
 
 const userCtrl = require('./controllers/users.controller');
 
 const marketplaceAddress = '0xc1ef465527343f68bb1841f99b9adeb061cc7ac9';
 const jinglesAddress = '0x5b6660ca047cc351bfedca4fc864d0a88f551485';
 
-const marketplaceContract = new web3.eth.Contract(marketplaceAbi.abi, marketplaceAddress);
+const marketplaceContract = new web3.eth.Contract(
+  marketplaceAbi.abi,
+  marketplaceAddress,
+);
 
 const jinglesContract = new web3.eth.Contract(jinglesAbi.abi, jinglesAddress);
 const jingleCtrl = require('./controllers/jingles.controller');
 
-const cryptoJinglesAddress = "0xdea4c5c25218994d0468515195622e25820d27c7";
-const cryptoJingles = new web3.eth.Contract(cryptoJinglesAbi.abi, cryptoJinglesAddress);
+const cryptoJinglesAddress = '0xdea4c5c25218994d0468515195622e25820d27c7';
+const cryptoJingles = new web3.eth.Contract(
+  cryptoJinglesAbi.abi,
+  cryptoJinglesAddress,
+);
 
-const startBlock = '13121891';
+const startBlock = '13153895';
+const endBlock = parseInt(startBlock, 10) + 2000;
+
+console.log(endBlock);
+
+// const endBlock = 'latest';
 
 async function update() {
-  jinglesContract.getPastEvents('Composed', { fromBlock: startBlock, toBlock: 'latest' }, async (err, events) => {
-
+  jinglesContract.getPastEvents(
+    'Composed',
+    { fromBlock: startBlock, toBlock: endBlock },
+    async (err, events) => {
       const mined = events;
 
       const numJinglesInDb = await jingleCtrl.jingleNum();
 
       console.log('Trying', mined.length, numJinglesInDb);
 
-     if (mined.length > numJinglesInDb) {
-      const newOnes = mined.length - numJinglesInDb;
+      if (mined.length > numJinglesInDb) {
+        const newOnes = mined.length - numJinglesInDb;
 
-      const jingles =
-                        mined
-                          .map(_jingle => _jingle.returnValues)
-                          .map(_jingle => ({ ..._jingle, jingleId: parseFloat(_jingle.jingleId.valueOf()) }))
-                          .map(_jingle => new Promise(async (resolve) => {
-                            const samples = _jingle.samples.map(s => s.valueOf());
-                            const sampleTypes = _jingle.jingleTypes.map(s => s.valueOf());
-                            const settings = _jingle.settings.map(s => s.valueOf());
+        const jingles = mined
+          .map((_jingle) => _jingle.returnValues)
+          .map((_jingle) => ({
+            ..._jingle,
+            jingleId: parseFloat(_jingle.jingleId.valueOf()),
+          }))
+          .map(
+            (_jingle) => new Promise(async (resolve) => {
+              const samples = _jingle.samples.map((s) => s.valueOf());
+              const sampleTypes = _jingle.jingleTypes.map((s) => s.valueOf());
+              const settings = _jingle.settings.map((s) => s.valueOf());
 
-                           if (_jingle.jingleId.valueOf() >= numJinglesInDb) {
-                            const saved = await jingleCtrl.addJingle({
-                              jingleId: _jingle.jingleId.valueOf(),
-                              name: _jingle.name,
-                              author: _jingle.author,
-                              owner: _jingle.owner,
-                              samples,
-                              sampleTypes,
-                              onSale: false,
-                              price: 0,
-                              settings,
-                            });
-                            resolve(saved);
-                            }
-                          }));
+              if (_jingle.jingleId.valueOf() >= numJinglesInDb) {
+                const saved = await jingleCtrl.addJingle({
+                  jingleId: _jingle.jingleId.valueOf(),
+                  name: _jingle.name,
+                  author: _jingle.author,
+                  owner: _jingle.owner,
+                  samples,
+                  sampleTypes,
+                  onSale: false,
+                  price: 0,
+                  settings,
+                });
+                resolve(saved);
+              }
+            }),
+          );
 
-      console.log('GET JINGLES SUCCESS', jingles);
+        console.log('GET JINGLES SUCCESS', jingles);
 
-      Promise.all(jingles).then((_jingles) => {
-        console.log('WROTE TO DB SUCCESS', _jingles);
-      });
+        Promise.all(jingles).then((_jingles) => {
+          console.log('WROTE TO DB SUCCESS', _jingles);
+        });
       }
-    });
+    },
+  );
 
-
-    cryptoJingles.getPastEvents('Purchased', { fromBlock: startBlock, toBlock: 'latest' }, async (err, ress) => {
-
+  cryptoJingles.getPastEvents(
+    'Purchased',
+    { fromBlock: startBlock, toBlock: endBlock },
+    async (err, ress) => {
       ress.forEach(async (res) => {
         const address = res.returnValues.user;
         const numSamples = res.returnValues.numJingles.valueOf();
@@ -82,49 +103,55 @@ async function update() {
 
         await userCtrl.addUser(address, numSamples);
       });
-  });
+    },
+  );
 
   const marketplace = {};
 
-  marketplaceContract.getPastEvents('allEvents', { fromBlock: startBlock, toBlock: 'latest' }, async (err, events) => {
-    console.log(err, events);
-    addMarketplaceEvents(events, marketplace);
+  marketplaceContract.getPastEvents(
+    'allEvents',
+    { fromBlock: startBlock, toBlock: endBlock },
+    async (err, events) => {
+      console.log(err, events);
+      // eslint-disable-next-line no-use-before-define
+      addMarketplaceEvents(events, marketplace);
 
-    for (const key of Object.keys(marketplace)) {
-      const eventList = marketplace[key];
+      for (const key of Object.keys(marketplace)) {
+        const eventList = marketplace[key];
 
-      let numOrders = 0;
-      let numCancels = 0;
+        let numOrders = 0;
+        let numCancels = 0;
 
-      eventList.forEach((e) => {
-        if (e.event === 'SellOrder') {
-          numOrders++;
+        eventList.forEach((e) => {
+          if (e.event === 'SellOrder') {
+            numOrders++;
+          } else {
+            numCancels++;
+          }
+        });
+
+        const e = eventList[eventList.length - 1];
+
+        if (numOrders > numCancels) {
+          // add
+          await putOnSale(eventList[eventList.length - 1]);
         } else {
-          numCancels++;
-        }
-      });
+          // cancel
 
-      const e = eventList[eventList.length - 1];
-
-      if (numOrders > numCancels) {
-        // add
-        await putOnSale(eventList[eventList.length - 1]);
-      } else {
-        // cancel
-
-        if (e.event === 'Bought') {
-          await boughtJingle(e);
-        } else {
-          await cancelJingle(e);
+          if (e.event === 'Bought') {
+            await boughtJingle(e);
+          } else {
+            await cancelJingle(e);
+          }
         }
       }
-    }
-  });
+    },
+  );
 }
 
 async function boughtJingle(res) {
   const jingleId = res.returnValues.jingleId.valueOf();
-  const buyer = res.returnValues.buyer;
+  const { buyer } = res.returnValues;
 
   const updated = await jingleCtrl.removeFromSale(jingleId, buyer);
 
@@ -148,7 +175,7 @@ async function putOnSale(res) {
 
 async function cancelJingle(res) {
   const jingleId = res.returnValues.jingleId.valueOf();
-  const owner = res.returnValues.owner;
+  const { owner } = res.returnValues;
 
   const updated = await jingleCtrl.removeFromSale(jingleId, owner);
 
@@ -156,7 +183,6 @@ async function cancelJingle(res) {
     console.log('Sell Order removed (canceled)');
   }
 }
-
 
 function addMarketplaceEvents(events, marketplace) {
   events.forEach((event) => {
@@ -172,7 +198,7 @@ function addMarketplaceEvents(events, marketplace) {
 }
 
 (async () => {
-setInterval(async () => {
+  // setInterval(async () => {
   update();
-}, 1000*60*2);
+  // }, 1000*60*2);
 })();
