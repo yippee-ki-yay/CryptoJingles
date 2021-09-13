c44525586fe796e00ac95a449e2ff9df5251f7dc
astankovic <aleksa.stankovic@berkeley.edu>
2021-09-13 16:57:31 +0200

    Fix opensea link.

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => (
 
           <a
             className="reddit"
-            href="https://opensea.io/collection/new-jingles"
+            href="https://opensea.io/collection/new-cryptojingles"
             target="_blank"
             rel="noopener noreferrer"
           >
